6c3f5aaa4851600df4e538de5450ca8bcfa3ead7
Andre Merzky <andre@merzky.net>
2018-06-18 16:43:20 +0200

    add rp timing script

---
+++ examples/00_rp.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+import radical.pilot as rp
+
+
+# ------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+    times   = list()
+    session = None
+    n       = 1
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+
+    try:
+
+        times.append(time.time())
+        session = rp.Session()
+        pmgr    = rp.PilotManager(session=session)
+        umgr    = rp.UnitManager(session=session)
+
+        times.append(time.time())
+        times.append(time.time())
+        pd_init = {'resource'      : 'local.localhost',
+                   'runtime'       : 20,
+                   'exit_on_error' : True,
+                   'cores'         : 160
+                  }
+        pdesc = rp.ComputePilotDescription(pd_init)
+        pilot = pmgr.submit_pilots(pdesc)
+        umgr.add_pilots(pilot)
+        times.append(time.time())
+
+        cuds = list()
+        for i in range(0, n):
+            cud = rp.ComputeUnitDescription()
+            cud.executable       = '/bin/true'
+            cud.cpu_processes    = 1
+            cuds.append(cud)
+        umgr.submit_units(cuds)
+        times.append(time.time())
+
+        umgr.wait_units()
+        times.append(time.time())
+
+        pilot.cancel()
+        times.append(time.time())
+
+
+    finally:
+        if session:
+            session.close()
+
+    times.append(time.time())
+    data = '%6d \t %s\n' % (n, ''.join(['\t%6.2f' % (t - times[0]) 
+                                        for t in times]))
+    with open('nge.dat', 'a+') as fout:
+        print data
+        fout.write(data)
+
+# ------------------------------------------------------------------------------
+
